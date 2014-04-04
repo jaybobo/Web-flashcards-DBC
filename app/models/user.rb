054c8336d57bfe2e1cbc9164288604764f4846e3
Apprentice <apprentice@dbc-chicago-023.local>
2014-04-04 18:37:10 -0500

    changed password model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  include BCrypt
+
   has_many :rounds
+
+  has_secure_password
+
   validates :user_email, presence: true
   validates :user_email, uniqueness: true
   validates :user_name,  presence: true
+
   validates :user_name,  uniqueness: true
-  validates :user_password,  uniqueness: true
-  validates :user_password,  presence: true
 end
--- db/migrate/20140404144731_create_users.rb
+++ db/migrate/20140404144731_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
       create_table :users do |t|
         t.string   :user_name
         t.string   :user_email
-        t.string   :user_password
+        t.string   :password_digest
       end
   end
 end
