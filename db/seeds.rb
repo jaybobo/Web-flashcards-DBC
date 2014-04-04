8bdfe55504dce90cb0d6758de279763bfd5975b8
jaybobo <info@goodproduce.net>
2014-04-04 16:13:48 -0500

    Seeds single database with name "State Capitals" and adds the id to all respective cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,16 @@
 require 'csv'
 
-
 FILE = 'statecapitals.csv'
 
-
-class Card
-	def initialize(args)
-		@question = args[:question]
-		@answer = args[:answer]
-	end
-end
-
-
 options = { :headers => true, :header_converters => :symbol, :converters => :all }
+
+
+deck = Deck.create(name: "State Capitals")
 
 #seed_question = "What is the capital of " + state
 
 	CSV.foreach(FILE, options) do |row|
-		Card.new(question: row[:state], answer: row[:capital])
+		Card.new(question: row[:state], answer: row[:capital], deck_id: deck.id)
 	end
-
-
-#:header_converters => :downcase,
-
-# def parser(filename, options)
-# parser(FILE, options)
-
-
-
-#:headers => true, :header_converter => :symbols
-#seed_question = "What is the capital of " + state
-#seed_answer = capital
-
-
-#deck = Deck.new
-#Card.new(deck_id: deck, question: seed_question, answer: seed_answer)
-
-
-
 
 
