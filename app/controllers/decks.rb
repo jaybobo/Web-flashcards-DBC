f008f828e98f14e6a567d460cf909523d9f86ed2
harry <apprentice@dbc-chicago-022.local>
2014-04-05 14:39:43 -0500

    add cards_correct and cards_answered to the rounds table. having trouble in the views card_counter where the first question repeats before moving on.

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,7 +1,53 @@
-get '/decks' do
+get '/decks/:id' do
+  #add auth
+  @deck = Deck.find(params[:id])
+  @new_round = true
   erb :"/decks/show"
 end
 
-get '/decks/gameover' do
-  erb :'/decks/gameover'
+post '/decks/:id/new_game' do
+  @deck = Deck.find(params[:id])
+  @round = Round.create(user_id: session[:value], deck_id: @deck.id)
+  @new_round = false
+  @card_count = 0
+  @correct = 0
+  @card = @deck.cards[@card_count]
+  erb :"/decks/show"
 end
+
+post '/decks/:id/next' do
+  @deck = Deck.find(params[:id])
+  @round = Round.where(user_id: session[:value], deck_id: @deck.id).last
+  @user_guess = params[:user_guess]
+
+
+
+  if @user_guess == @card.answer
+    # @correct += 1
+    @guess = Guess.create(
+      round_id: @round.id,
+      card_id: @card.id,
+      user_guess: @user_guess,
+      correctness: true
+      )
+    if @card.id == @deck.cards.last.id
+      erb :'/decks/gameover'
+    end
+    erb :'/decks/show'
+  else
+    @guess = Guess.create(
+      round_id: @round.id,
+      card_id: @card.id,
+      user_guess: @user_guess,
+      correctness: false
+      )
+    if @card.id == @deck.cards.last.id
+      erb :'/decks/gameover'
+    end
+    erb :'/decks/show'
+  end
+end
+
+
+
+
