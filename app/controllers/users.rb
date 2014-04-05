ffafca5e61a66721bafea5ec058f96f08f853342
harry <apprentice@dbc-chicago-022.local>
2014-04-04 21:05:36 -0500

    added user login and logout routes

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -12,8 +12,28 @@ get '/users/new' do
   erb :"users/new"
 end
 
-get 'users/:id' do
-  redirect to ('/users')
+# get '/users/:id' do
+#   redirect to ('/users')
+# end
+
+get '/users/login' do
+  erb :"users/login"
+end
+
+get '/users/logout' do
+  session.clear
+  redirect to '/'
+end
+
+post '/users/login' do
+  @user = User.find_by(user_email: params[:user_email])
+
+  if @user && @user.authenticate(params[:password])
+    session[:value] = @user.id
+    erb :"/users/index"
+  else
+    erb :"users/login"
+  end
 end
 
 post '/users' do
