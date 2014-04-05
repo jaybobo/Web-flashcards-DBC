b9d8ef08d52b13aaf6ca6023734b3b8e6f5a25a2
harry <apprentice@dbc-chicago-022.local>
2014-04-04 20:31:14 -0500

    added new user post method

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,27 @@
+enable :sessions
+
 get '/users' do
   erb :"users/index"
+end
+
+get '/users/' do
+  erb :"users/index"
+end
+
+get '/users/new' do
+  erb :"users/new"
+end
+
+get 'users/:id' do
+  redirect to ('/users')
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:value] = User.last.id
+    redirect to ('/users')
+  else
+    erb :"users/new"
+  end
 end
