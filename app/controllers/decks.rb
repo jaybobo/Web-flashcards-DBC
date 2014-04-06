e9bc6aa1620d6d22fcda6fe31f05fb2d898b31b1
harry <apprentice@dbc-chicago-022.local>
2014-04-05 21:06:16 -0500

    fix gameover page, now game over page has a table showing the questions, correct answers and user's answers

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,14 +1,4 @@
 get '/decks/:id' do
-  #add auth
-  # @user = User.find_by(id: session[:value])
-  # if @user
-  #   @deck = Deck.find(params[:id])
-  #   @new_round = true
-  #   erb :"/decks/new_game"
-  # else
-  #   redirect to('/')
-  # end
-
   if session[:value]
     @deck = Deck.find(params[:id])
     @new_round = true
@@ -18,10 +8,10 @@ get '/decks/:id' do
   end
 end
 
-
 post '/decks/:id/next' do
   @deck = Deck.find(params[:id])
   @first_question = params[:first_question]
+
   if @first_question == "yes"
     @round = Round.create(user_id: session[:value], deck_id: @deck.id)
   else
@@ -31,23 +21,13 @@ post '/decks/:id/next' do
   @round.update_attributes(cards_played: @round.cards_played)
   @user_guess = params[:user_guess]
   @card = @deck.cards[@round.cards_played]
-  # p "This is next, here is @round.cards_played: #{@round.cards_played}"
-  # p "This is next, here is @card.question #{@card.question}"
-  # p "This is next, here is @deck.cards[@round.cards_played - 1].answer #{@deck.cards[@round.cards_played - 1].answer}"
-  # p "This is next, here is @user_guess: #{@user_guess.inspect}"
 
   if @user_guess.nil?
     nil_answer
   elsif @user_guess == @deck.cards[@round.cards_played - 1].answer
     correct_answer
-    if @card.nil?
-      erb :'/decks/gameover'
-    end
   else
     wrong_answer
-    if @card.nil?
-      erb :'/decks/gameover'
-    end
   end
   erb :'/decks/show'
 end
