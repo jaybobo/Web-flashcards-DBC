0a9738b4ce438a176b6c7ecbe655e4b4ccb4d61c
Harry Wang <hgw2101@gmail.com>
2014-04-05 19:40:03 -0500

    v

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -2,37 +2,50 @@ get '/decks/:id' do
   #add auth
   @deck = Deck.find(params[:id])
   @new_round = true
-  erb :"/decks/show"
+  erb :"/decks/new_game"
 end
 
-post '/decks/:id/new_game' do
-  @deck = Deck.find(params[:id])
-  @round = Round.create(user_id: session[:value], deck_id: @deck.id)
-  @new_round = false
-  @card = @deck.cards[@round.cards_played]
-  @round.update_attributes(cards_played: @round.cards_played + 1)
-  erb :"/decks/show"
-end
+# post '/decks/:id/new_game' do
+#   @deck = Deck.find(params[:id])
+#   @round = Round.create(user_id: session[:value], deck_id: @deck.id)
+#   @card = @deck.cards[@round.cards_played]
+#   # @round.update_attributes(cards_played: @round.cards_played + 1)
+#   p "This is new_game, here is @card.question: #{@card.question}"
+#   p "This is new_game, here is @round.cards_played: #{@round.cards_played}"
+#   erb :"/decks/show"
+# end
 
 post '/decks/:id/next' do
   @deck = Deck.find(params[:id])
-  @round = Round.where(user_id: session[:value], deck_id: @deck.id).last
+  @first_question = params[:first_question]
+  if @first_question == "yes"
+    @round = Round.create(user_id: session[:value], deck_id: @deck.id)
+  else
+    @round = Round.where(user_id: session[:value], deck_id: @deck.id).last
+  end
+
+  @round.update_attributes(cards_played: @round.cards_played)
   @user_guess = params[:user_guess]
-  @card = @deck.cards[@round.cards_played - 1]
-  # p @deck.cards[@round.cards_played -1 ].answer
-  if @user_guess == @card.answer
+  @card = @deck.cards[@round.cards_played]
+  # p "This is next, here is @round.cards_played: #{@round.cards_played}"
+  # p "This is next, here is @card.question #{@card.question}"
+  # p "This is next, here is @deck.cards[@round.cards_played - 1].answer #{@deck.cards[@round.cards_played - 1].answer}"
+  # p "This is next, here is @user_guess: #{@user_guess.inspect}"
+
+  if @user_guess.nil?
+    nil_answer
+  elsif @user_guess == @deck.cards[@round.cards_played - 1].answer
     correct_answer
     if @card.id == @deck.cards.last.id
       erb :'/decks/gameover'
     end
-    erb :'/decks/show'
   else
     wrong_answer
     if @card.id == @deck.cards.last.id
       erb :'/decks/gameover'
     end
-    erb :'/decks/show'
   end
+  erb :'/decks/show'
 end
 
 
